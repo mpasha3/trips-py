aa78fc413f76985a1edc3147cf817aa6c4f39c65
mpasha3 <mpasha3@asu.edu>
2023-06-07 13:55:37 -0400

    added MINRES

---
+++ trips/solvers/MINRES.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+Definition of test problems
+--------------------------------------------------------------------------
+Created Jun 7, 2023 for TRIPs-Py library
+"""
+__authors__ = "Mirjeta Pasha and Connor Sanderford"
+__copyright__ = "Copyright 2022, TRIPs-Py library"
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Mirjeta Pasha"
+__email__ = "mirjeta.pasha@tufts.edu; mirjeta.pasha1@gmail.com"
+
+from select import select
+from ..decompositions import generalized_golub_kahan
+import numpy as np
+from scipy import linalg as la
+
+from tqdm import tqdm
+def MINRES(A, b, projection_dim=3, dp_stop = 0, **kwargs):
+    if A.shape[0] == A.shape[1]:
+        b_vec = b.reshape((-1,1))
+        (U, B, V) = generalized_golub_kahan(A, b_vec, n_iter = 10, projection_dim = projection_dim)
+        VV = V
+        UU = U[:, 0:-1]
+        HH = B[0:-1, :]
+        bhat = UU.T.dot(b_vec)
+        y = np.linalg.solve(HH.T*HH, bhat)
+        x_GKahan = VV.dot(y)
+    return (x_GKahan)
