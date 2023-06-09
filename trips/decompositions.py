61df06fffd3b8e64fd8ac7acaaf463c34be3b51a
connor sanderford <connorsanderford@gmail.com>
2023-06-09 03:51:24 -0700

    dp stop for arnoldi

--- trips/decompositions.py
+++ trips/decompositions.py
@@ -8,41 +8,73 @@ from scipy.optimize import newton
 
 from tqdm import tqdm
 
-def arnoldi(A: 'np.ndarray[np.float]', n: int, q_0: 'np.ndarray[np.float]' ) -> 'Tuple[np.ndarray[np.float], np.ndarray[np.float]]':
+def arnoldi(A: 'np.ndarray[np.float]', b: 'np.ndarray[np.float]', n_iter: int, dp_stop=False, **kwargs ) -> 'Tuple[np.ndarray[np.float], np.ndarray[np.float]]':
     """
     computes the rank-n Arnoldi factorization of A, with initial guess q_0.
 
     returns Q (m x n), an orthonormal matrix, and H (n+1 x n), an upper Hessenberg matrix.
     """
 
+    eta = kwargs['gk_eta'] if ('gk_eta' in kwargs) else 1.001
+    delta = kwargs['gk_delta'] if ('gk_delta' in kwargs) else 0.001
+
+    (rows, cols) = A.shape
+
     # preallocate
 
-    Q = np.zeros((A.shape[0], n+1))
-    H = np.zeros((n+1, n))
+    Q = np.zeros((rows, 1+1))
+    H = np.zeros((cols, 1))
 
-    # normalize q_0
-    q_0 = q_0/np.linalg.norm(q_0, ord=2)
+    # normalize b
+    b = b/np.linalg.norm(b, ord=2)
 
-    # q_0 is first basis vector
-    Q[:, 0] = q_0[:,0]
+    # b is first basis vector
+    Q[:, 0] = b[:,0]
 
-    for ii in tqdm(range(0,n), desc = "generating basis..."): # for each iteration over the method:
+    iterations = 0
 
-        q_nplus1  = A @ Q[:,ii] # generate the next vector in the Krylov subspace
+    res_norm = np.inf
 
-        for jj in range(0,n): # for each iteration *that has been previously completed*:
+    for ii in tqdm(range(0,n_iter), desc = "generating basis..."): # for each iteration over the method:
 
-            H[jj,ii] = np.dot( Q[:,jj], q_nplus1 ) # calculate projections of the new Krylov vector onto previous basis elements
+        if ((dp_stop==True) and (res_norm <= eta*delta)):
 
-            q_nplus1 = q_nplus1 - H[jj,ii] * Q[:,jj] # and orthogonalize the new Krylov vector with respect to previous basis elements
+            print('discrepancy principle satisfied, stopping early.')
+            
+            break
 
-        if ii < n:
-            H[ii+1, ii] = np.linalg.norm(q_nplus1, 2)
+        if iterations != 0:
+            Q = np.pad(Q, ((0,0), (0,1)) )  # at each iteration that doesn't satisfy the discrepancy principle,
+            H = np.pad(H, ((0,0), (0,1)) )  # add an additional column to the bases and entry to the bidiagonal entries.
+
+
+        b_nplus1  = A @ Q[:,ii] # generate the next vector in the Krylov subspace
+
+        for jj in range(0,iterations): # for each iteration *that has been previously completed*:
+
+            H[jj,ii] = np.dot( Q[:,jj], b_nplus1 ) # calculate projections of the new Krylov vector onto previous basis elements
+
+            b_nplus1 = b_nplus1 - H[jj,ii] * Q[:,jj] # and orthogonalize the new Krylov vector with respect to previous basis elements
+
+        if ii < n_iter:
+            H[ii+1, ii] = np.linalg.norm(b_nplus1, 2)
 
             if H[ii+1,ii] == 0:
                 return (Q,H)
 
-            Q[:, ii+1] = q_nplus1/H[ii+1,ii]
+            Q[:, ii+1] = b_nplus1/H[ii+1,ii]
+
+        if (dp_stop == True):
+
+            bhat = Q[:,:-1].T @ b
+
+            y = np.linalg.lstsq(H[:-1,:].T @ H[:-1,:], bhat, rcond=None)[0]
+
+            x = Q[:,:-1] @ y
+
+            res_norm = np.linalg.norm(A @ x - b)
+
+        iterations += 1
 
 
     return (Q,H)
@@ -56,7 +88,7 @@ def generalized_golub_kahan(A, b, n_iter, dp_stop=False, **kwargs):
     (rows, cols) = A.shape
 
     betas = np.zeros(shape=(1)) # start the set of alphas and betas in the bidiagonal matrix
-    alphas = np.zeros(shape=(1)) # return matrix instead of aarrays
+    alphas = np.zeros(shape=(1)) # return matrix instead of arrays
 
     U = np.zeros((rows, 1+1)) # start the bases at dimension of iter and iter+1
     V = np.zeros((cols, 1))
