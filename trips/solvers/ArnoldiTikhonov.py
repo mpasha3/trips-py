c7a0ef15c6e9ef1aa8e649628de1665a6fda368e
mpasha3 <mpasha3@asu.edu>
2023-06-23 17:57:17 +0100

    Added AT

--- trips/solvers/ArnoldiTikhonov.py
+++ trips/solvers/ArnoldiTikhonov.py
@@ -19,43 +19,23 @@ from trips.parameter_selection.gcv import *
 from trips.parameter_selection.discrepancy_principle import *
 def Arnoldi_Tikhonov(A, b, L = 'Identity', projection_dim=3, iter=50, dp_stop = 0, param_choice = 'manual', automatic_param = 'gcv', reg_param = 1, delta = 0, **kwargs):
     if A.shape[0] == A.shape[1]:
-        if L == 'Identity': ## This is Arnoldi Tikhonov in standard form
-            b_vec = b.reshape((-1,1))
-            (V,H) = arnoldi(A, projection_dim, b_vec, **kwargs)
-            UU = V[:, 0:-1]
-            HH = H[0:-1, :]
-            bhat = UU.T.dot(b_vec)
-            if param_choice == 'manual':
-                y = np.linalg.solve(HH.T*HH + reg_param*L, bhat)
-                x_AT = UU.dot(y)
-            else:
-                if automatic_param == 'gcv':
-                    L = np.identity(HH.shape[1], dtype='float32')
-                    reg_param = generalized_crossvalidation(HH, bhat, L, **kwargs)['x'] # find ideal lambda by crossvalidation
-                else:
-                    reg_param = discrepancy_principle(HH, bhat, L, eta = 1.01, noise_norm = delta, **kwargs)['x'][0] # find ideal lambdas by crossvalidation
-                    L = np.identity(HH.shape[1], dtype='float32')
-                y = np.linalg.solve(HH.T*HH + reg_param[0]*L, bhat)
-                x_AT = UU.dot(y)
+        b_vec = b.reshape((-1,1))
+        (V,H) = arnoldi(A, b_vec, projection_dim, dp_stop)
+        UU = V[:, 0:-1]
+        HH = H[0:-1, :]
+        bhat = UU.T.dot(b_vec)
+        if param_choice == 'manual':
+            y = np.linalg.solve(HH.T@HH + reg_param*(Identity(HH.shape[1]).todense()), bhat)
+            x_AT = UU.dot(y)
         else:
-            b_vec = b.reshape((-1,1))
-            (V,H) = arnoldi(A, projection_dim, b_vec, **kwargs)
-            # L = np.identity(H.shape[1], dtype='float32')
-            UU = V[:, 0:-1]
-            HH = H[0:-1, :]
-            bhat = UU.T.dot(b_vec)
-            if param_choice == 'manual':
-                y = np.linalg.solve(HH.T*HH + reg_param*L, bhat)
-                x_AT = UU.dot(y)
+            if automatic_param == 'gcv':
+                L = np.identity(HH.shape[1], dtype='float32')
+                reg_param = generalized_crossvalidation(HH, bhat, L, **kwargs)['x'] # find ideal lambda by crossvalidation
             else:
-                if automatic_param == 'gcv':
-                    L = np.identity(HH.shape[1], dtype='float32')
-                    reg_param = generalized_crossvalidation(HH, bhat, L, **kwargs)['x'] # find ideal lambda by crossvalidation
-                else:
-                    L = np.identity(HH.shape[1], dtype='float32')
-                    reg_param = discrepancy_principle(HH, bhat, L, 1.01, delta, **kwargs)['x'] # find ideal lambdas by crossvalidation
-                y = np.linalg.solve(HH.T*HH + reg_param[0]*L, bhat)
-                x_AT = UU.dot(y)
+                reg_param = discrepancy_principle(HH, bhat, L, eta = 1.01, noise_norm = delta, **kwargs)['x'][0] # find ideal lambdas by crossvalidation
+                L = np.identity(HH.shape[1], dtype='float32')
+            y = np.linalg.solve(HH.T*HH + reg_param[0]*L, bhat)
+            x_AT = UU.dot(y)
     else:
         raise Warning("The matrix is not square. Arnoldi can not be applied")
     return (x_AT)
