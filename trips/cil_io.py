fad83dbca47c96919df8905d875096c3f5df74f9
connor sanderford <connorsanderford@gmail.com>
2023-06-23 02:28:42 -0700

    move STEMPO function from io to cil_io

--- trips/cil_io.py
+++ trips/cil_io.py
@@ -8,6 +8,8 @@ import os
 from os import mkdir
 from os.path import exists
 
+import astra
+
 from cil.framework import AcquisitionGeometry
 from cil.plugins.astra.operators import ProjectionOperator
 from cil.processors import Slicer
@@ -207,3 +209,122 @@ def gen_gelPhantom():
         temp = ((data.array)[i, :, :]).flatten()
         B[:, i] = temp
     return A, data, AA, B, n_x, n_y, n_t, ig, ag, ig_small, ag_small
+
+
+def get_stempo_data(data_set = 'real', data_thinning = '2'):
+        """
+        Generate stempo observations
+        """
+        data_file = {'simulation':'stempo_ground_truth_2d_b4','real':'stempo_seq8x45_2d_b'+str(data_thinning)}[data_set]+'.mat'
+        if not os.path.exists('./data/stempo_data'): os.makedirs('./data/stempo_data')
+        if not os.path.exists('./data/stempo_data/'+data_file):
+            import requests
+            print("downloading...")
+            r = requests.get('https://zenodo.org/record/7147139/files/'+data_file)
+            with open('./data/'+data_file, "wb") as file:
+                file.write(r.content)
+            print("Stempo data downloaded.")
+        if  data_set=='simulation':
+            truth = spio.loadmat('./data/stempo_data/'+data_file)
+            image = truth['obj']
+            nx, ny, nt = 560, 560, 20
+            anglecount = 10
+            rowshift = 5
+            columnsshift = 14
+            nt = 20
+            angleVector = list(range(nt))
+            for t in range(nt):
+                angleVector[t] = np.linspace(rowshift*t, 14*anglecount+ rowshift*t, num = anglecount+1)
+            angleVectorRad = np.deg2rad(angleVector)
+                    # Generate matrix versions of the operators and a large bidiagonal sparse matrix
+            N = nx         # object size N-by-N pixels
+            p = int(np.sqrt(2)*N)    # number of detector pixels
+            # view angles
+            theta = angleVectorRad#[0]#np.linspace(0, 2*np.pi, q, endpoint=False)   # in rad
+            q = theta.shape[1]          # number of projection angles
+            source_origin = 3*N                     # source origin distance [cm]
+            detector_origin = N                       # origin detector distance [cm]
+            detector_pixel_size = (source_origin + detector_origin)/source_origin
+            detector_length = detector_pixel_size*p 
+            saveA = list(range(nt))
+            saveb = np.zeros((p*q, nt))
+            saveb_true = np.zeros((p*q, nt))
+            savee = np.zeros((p*q, nt))
+            savedelta = np.zeros((nt, 1))
+            savex_true = np.zeros((nx*ny, nt))
+            B = list(range(nt))
+            count = np.int_(360/nt)
+            for i in range(nt):
+                proj_geom = astra.create_proj_geom('fanflat', detector_pixel_size, p, theta[i], source_origin, detector_origin)
+                vol_geom = astra.create_vol_geom(N, N)
+                proj_id = astra.create_projector('line_fanflat', proj_geom, vol_geom)
+                mat_id = astra.projector.matrix(proj_id)
+                A_n = astra.matrix.get(mat_id)
+                x_true = image[:, :, count*i]
+                x_truef_sino = x_true.flatten(order='F') 
+                savex_true[:, i] = x_truef_sino
+                sn = A_n*x_truef_sino
+                b_i = sn.flatten(order='F') 
+                sigma = 0.01 # noise level
+                e = np.random.normal(0, 1, b_i.shape[0])
+                e = e/np.linalg.norm(e)*np.linalg.norm(b_i)*sigma
+                delta = np.linalg.norm(e)
+                b_m = b_i + e
+                saveA[i] = A_n
+                B[i] = b_m
+                saveb_true[:, i] = sn
+                saveb[:, i] = b_m
+                savee[:, i] = e
+                savedelta[i] = delta
+                astra.projector.delete(proj_id)
+                astra.matrix.delete(mat_id)
+            A = sps.block_diag((saveA))    
+            b = saveb.flatten(order ='F') 
+            # xf = savex_true.flatten(order = 'F')
+            truth = savex_true.reshape((nx, ny, nt), order='F').transpose((2,0,1))
+        elif data_set=='real':
+            import h5py
+            N = int(2240/data_thinning) # 140
+            nx, ny, nt =  N, N, 8
+            N_det = N
+            N_theta = 45
+            theta = np.linspace(0,360,N_theta,endpoint=False)
+            # Load measurement data as sinogram
+            # data = spio.loadmat('./data/'+data_file) # scipy.io does not support Matlab struct
+            data = h5py.File('./data/stempo_data/'+data_file,'r')
+            CtData = data["CtData"]
+            m = np.array(CtData["sinogram"]).T # strange: why is it transposed?
+            # Load parameters
+            param = CtData["parameters"]
+            f = h5py.File('A_seqData.mat') # it is created by `create_ct_matrix_2d_fan_astra.m` separately
+            fA = f["A"]
+            # Extract information
+            Adata = np.array(fA["data"])
+            Arowind = np.array(fA["ir"])
+            Acolind = np.array(fA["jc"])
+            # Need to know matrix size (shape) somehow
+            n_rows = N_det*N_theta # 6300
+            n_cols = N*N
+            Aloaded = sps.csc_matrix((Adata, Arowind, Acolind), shape=(n_rows, n_cols))
+            saveA = list(range(nt))
+            saveb = np.zeros((n_rows, nt))
+            savee = np.zeros((n_rows, nt))
+            savedelta = np.zeros((nt, 1))
+            B = list(range(nt))
+            for i in range(nt):
+                tmp = m[45*(i):45*(i+1), :]
+                b_i = tmp.flatten()
+                sigma = 0.01 # noise level
+                e = np.random.normal(0, 1, b_i.shape[0])
+                e = e/np.linalg.norm(e)*np.linalg.norm(b_i)*sigma
+                delta = np.linalg.norm(e)
+                b_m = b_i + e
+                saveA[i] = Aloaded
+                B[i] = b_m
+                saveb[:, i] = b_m
+                savee[:, i] = e
+                savedelta[i] = delta
+            A = sps.block_diag((saveA))    
+            b = saveb.flatten(order ='F')
+            truth = None
+        return A, b, saveA, B, nx, ny, nt, savedelta, truth
