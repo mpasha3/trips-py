7a5f107226ef4ef9975de94a1dfcf8b26d4e260e
mpasha3 <mpasha3@asu.edu>
2023-06-27 13:11:44 +0100

    add delta in AnisoTV

--- trips/solvers/AnisoTV.py
+++ trips/solvers/AnisoTV.py
@@ -17,7 +17,7 @@ from matplotlib import transforms
 from scipy import ndimage
 from trips.solvers.gks import *
 
-def AnisoTV(A, b, AA, B, nx, ny, nt, dynamic, iters, reg_param, testproblem):
+def AnisoTV(A, b, AA, B, nx, ny, nt, dynamic, iters, reg_param, testproblem, noise_norm):
     """
     Reconstruct images by Anisotropic Total Variation
     """
@@ -25,20 +25,20 @@ def AnisoTV(A, b, AA, B, nx, ny, nt, dynamic, iters, reg_param, testproblem):
         if dynamic == True:
             b_vec = b.reshape((-1,1))
             L = time_derivative_operator(nx, ny, nt)
-            (x, x_history, lambdah, lambda_history) = MMGKS(A, b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None)
+            (x, x_history, lambdah, lambda_history) = MMGKS(A, b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None, delta = noise_norm)
 #            xx = np.reshape(x, (nx, ny, nt), order="F")
         else:
             xx = list(range(nt))
             L = spatial_derivative_operator(nx, ny, 1)
             for i in range(nt):
                 b_vec = B[:, i].reshape((-1,1))
-                (x, x_history, lambdah, lambda_history) = MMGKS(AA[i], b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None)
+                (x, x_history, lambdah, lambda_history) = MMGKS(AA[i], b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None, delta = noise_norm)
                 xx[i] = x
     elif testproblem == 'Test':
         if dynamic == True:
             b_vec = b.reshape((-1,1))
             L = time_derivative_operator(nx, ny, nt)
-            (x, x_history, lambdah, lambda_history) = MMGKS(A, b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None)
+            (x, x_history, lambdah, lambda_history) = MMGKS(A, b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None, delta = noise_norm)
             xx = np.reshape(x, (nx, ny, nt), order="F")
         else:
             xx = list(range(nt))
@@ -46,14 +46,14 @@ def AnisoTV(A, b, AA, B, nx, ny, nt, dynamic, iters, reg_param, testproblem):
             for i in range(nt):
                 print(i)
                 b_vec = B[i].reshape((-1,1))
-                (x, x_history, lambdah, lambda_history) = MMGKS(AA[i], b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None)
+                (x, x_history, lambdah, lambda_history) = MMGKS(AA[i], b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None, delta = noise_norm)
                 xx[i] = x
     else:
         if dynamic == True:
             b_vec = b.reshape((-1,1))
             L = spatial_derivative_operator(nx, ny, nt)
             # L = time_derivative_operator(nx, ny, nt)
-            (x, x_history, lambdah, lambda_history) = MMGKS(A, b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None)
+            (x, x_history, lambdah, lambda_history) = MMGKS(A, b_vec, L, pnorm=2, qnorm=1, projection_dim=1, n_iter = iters, regparam=reg_param, x_true=None, delta = noise_norm)
             xx = np.reshape(x, (nx, ny, nt), order="F")
         else:
             xx = list(range(nt))
@@ -61,7 +61,7 @@ def AnisoTV(A, b, AA, B, nx, ny, nt, dynamic, iters, reg_param, testproblem):
             L = first_derivative_operator_2d(nx, ny)
             for i in range(nt):
                 b_vec = B[i].reshape((-1,1))
-                (x, x_history, lambdah, lambda_history) = MMGKS(AA[i], b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None)
+                (x, x_history, lambdah, lambda_history) = MMGKS(AA[i], b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None, delta = noise_norm)
                 xx[i] = x
     return xx  
 
