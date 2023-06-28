4de205bc9c9b382ff2b179979e2aece7b847348e
mpasha3 <mpasha3@asu.edu>
2023-06-28 01:25:18 +0100

    ST

--- trips/solvers/AnisoTV.py
+++ trips/solvers/AnisoTV.py
@@ -34,7 +34,7 @@ def AnisoTV(A, b, AA, B, nx, ny, nt, dynamic, iters, reg_param, testproblem, del
                 b_vec = B[:, i].reshape((-1,1))
                 (x, x_history, lambdah, lambda_history) = MMGKS(AA[i], b_vec, L, pnorm=2, qnorm=1, projection_dim=3, n_iter = iters, regparam=reg_param, x_true=None, delta = delta)
                 xx[i] = x
-    elif testproblem == 'Stempo':
+    elif testproblem in ['STEMPO', 'Stempo', 'stempo']:
         if dynamic == True:
             b_vec = b.reshape((-1,1))
             L = time_derivative_operator(nx, ny, nt)
