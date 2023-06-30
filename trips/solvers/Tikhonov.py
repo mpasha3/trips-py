deb8ce6cdec0e003960072377c8baf527d375b2e
connor sanderford <connorsanderford@gmail.com>
2023-06-30 02:46:22 -0700

    warning for large observation operators

--- trips/solvers/Tikhonov.py
+++ trips/solvers/Tikhonov.py
@@ -5,6 +5,11 @@ from ..parameter_selection.discrepancy_principle import discrepancy_principle
 from ..utils import smoothed_holder_weights
 from collections.abc import Iterable
 def Tikhonov(A, b, L, x_true, regparam = 'gcv', **kwargs):
+
+    if max(A.shape[0], A.shape[1]) >= 100:
+
+        raise Warning("For large observation matrices A, this method will be slow. Consider using other solvers.")
+
     if regparam in ['gcv', 'GCV', 'Gcv']:
         lambdah = generalized_crossvalidation(A, b, L)['x'].item() # find ideal lambda by crossvalidation
         xTikh = np.linalg.solve(A.T@A + lambdah*L.T@L, A.T@b)
