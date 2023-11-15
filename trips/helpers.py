142cc7f1083d19c4b953364d2e28887be368f0c7
mpasha3 <mpasha3@asu.edu>
2023-11-15 17:38:05 -0500

    added get_image_data and generate_image_dat for getting the data from google drive

--- trips/helpers.py
+++ trips/helpers.py
@@ -90,14 +90,6 @@ def plot_singular_values_svd(Operator, parameter = 'A'):
         plt.savefig('singular_values.png', bbox_inches='tight')
         plt.show()
 
-def check_imagesize_toreshape(existingimage, chooseimage, old_size, newsize):
-    path_package = '/Users/mirjetapasha/Documents/Research_Projects/TRIPS_June25/multiparameter_package'
-    if (old_size[0] != newsize[0] or old_size[1] != newsize[1]):
-        Deblur.plot_rec(existingimage.reshape((shape), order = 'F'), save_imgs = False)
-        temp_im = Image.open(path_package + '/demos/data/images/'+chooseimage+'_'+str(newsize[0])+'.jpg')
-        image_new =  np.array(temp_im.resize((newsize[0], newsize[1])))
-        spio.savemat(path_package + '/demos/data/images/'+chooseimage+'_'+str(newsize[0])+'.mat', mdict={'x_true': image_new})
-    return image_new
 
 def plot_recstructions_series(img, shape, dynamic, testproblem, geome_x, geome_x_small, save_imgs= True, save_path='./reconstruction/Emoji'):
     """
--- trips/io.py
+++ trips/io.py
@@ -50,9 +50,47 @@ def build_x_true(dx=10, dy=10, up_width=10, bar_width=5, size=64):
     return x_exact
 
 """
+Images for 2D Deblurring
+"""
+def get_image_data(dataset = 'satellite'):
+
+    assert dataset in ['satellite', 'hubble', 'edges', 'star']
+
+    if exists(f'./data/image_data/{dataset}.mat'):
+        print('data already downloaded.')
+
+    else:
+        print("downloading...")
+
+        if not exists('./data'):
+            mkdir("./data")
+
+        if not exists('./data/emoji_data'):
+            mkdir("./data/emoji_data")
+
+        r = requests.get(f'https://zenodo.org/record/1183532/files/DataDynamic_128x{dataset}.mat')
+
+        with open(f'./data/emoji_data/DataDynamic_128x{dataset}.mat', "wb") as file:
+
+            file.write(r.content)
+
+        print("downloaded.")
+"""
+Generate image data
+"""
+def generate_image_dat(im):
+    get_image_data(im)
+    currentpath = os.getcwd()
+    f = spio.loadmat(f'./data/image_data/image_{im}.mat')
+    X = f['x_true']
+    X_true = X/X.max()
+    nx, ny = X_true.shape  
+    x_truef = X_true.reshape((-1,1))
+    return X_true
+
+"""
 Emoji data and operator:
 """
-
 def get_emoji_data(dataset = 30):
 
     assert dataset in [30,60]
@@ -87,37 +125,25 @@ def generate_emoji(noise_level, dataset):
         A = sparse.csc_matrix((f["A"]["data"], f["A"]["ir"], f["A"]["jc"]))
         normA = np.array(f['normA'])
         sinogram = np.array(f['sinogram']).T
-
     T = 33
     N = np.sqrt(A.shape[1] / T)
     [mm, nn] = sinogram.shape
-
     ind = []
-
     for ii in range(int(nn /3)):
-
         ind.extend( np.arange(0,mm) + (3*ii)*mm )
-
     m2 = sinogram[:, 0::3]
-
     A_small = A[ind, :]
-
     b = m2
     nt = int(T)
     nx = int(N)
     ny = int(N)
     b = b.reshape(-1, 1, order='F').squeeze()
-
     AA = list(range(T))
     B = list(range(T))
-
     delta = 0 # np.linalg.norm() # no added noise for this dataset, change to allow added noise.
-
     for ii in range(T):
-
         AA[ii] = A_small[ 2170*(ii):2170*(ii+1), 16384*ii:16384*(ii+1) ]
         B[ii] = b[ 2170*(ii) : 2170*(ii+1) ]
-
     return (A_small, b, AA, B, nx, ny, nt, delta)
 
 
