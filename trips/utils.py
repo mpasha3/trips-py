7d56747d8def84817cae87fc50270121d8e8c7f4
mpasha3 <mpasha3@asu.edu>
2023-11-15 11:34:47 -0500

    built check_imput_image_size and image_to_right_size for reshaping images. Now need to incorporate them in the Deblur()

--- trips/utils.py
+++ trips/utils.py
@@ -161,4 +161,82 @@ def check_operator_type(A):
         A = A.todense()
     return A
 
+# def check_imagesize_toreshape(existingimage, chooseimage, old_size, newsize):
+#     path_package = '/Users/mirjetapasha/Documents/Research_Projects/TRIPS_June25/multiparameter_package'
+#     if (old_size[0] != newsize[0] or old_size[1] != newsize[1]):
+#         Deblur.plot_rec(existingimage.reshape((shape), order = 'F'), save_imgs = False)
+#         temp_im = Image.open(path_package + '/demos/data/images/'+chooseimage+'_'+str(newsize[0])+'.jpg')
+#         image_new =  np.array(temp_im.resize((newsize[0], newsize[1])))
+#         spio.savemat(path_package + '/demos/data/images/'+chooseimage+'_'+str(newsize[0])+'.mat', mdict={'x_true': image_new})
+#     return image_new
+
+def check_imput_image_size(image):
+    imshape = image.shape
+    if imshape[1] == 1:
+        nx = int(np.sqrt(imshape[0]))
+        ny = int(np.sqrt(imshape[0]))
+    else:
+        nx = imshape[0]
+        ny = imshape[1]
+    newshape = (nx, ny)
+    return newshape
+
+def image_to_right_size(image, n):
+    X, Y = np.meshgrid(np.linspace(1, image.shape[1], n[0]), np.linspace(1, image.shape[0], n[1]))
+    im = interp2linear(image, X, Y, extrapval=np.nan)
+
+def interp2linear(z, xi, yi, extrapval=np.nan):
+    """
+    This function is obtained from this github repository: https://github.com/serge-m/pyinterp2 to be used for automatically reshaping the images
+    __author__ = 'Sergey Matyunin'
+    Linear interpolation equivalent to interp2(z, xi, yi,'linear') in MATLAB
+    @param z: function defined on square lattice [0..width(z))X[0..height(z))
+    @param xi: matrix of x coordinates where interpolation is required
+    @param yi: matrix of y coordinates where interpolation is required
+    @param extrapval: value for out of range positions. default is numpy.nan
+    @return: interpolated values in [xi,yi] points
+    @raise Exception:
+    """
+    x = xi.copy()
+    y = yi.copy()
+    nrows, ncols = z.shape
+    if nrows < 2 or ncols < 2:
+        raise Exception("z shape is too small")
+    if not x.shape == y.shape:
+        raise Exception("sizes of X indexes and Y-indexes must match")
+    # find x values out of range
+    x_bad = ( (x < 0) | (x > ncols - 1))
+    if x_bad.any():
+        x[x_bad] = 0
+    # find y values out of range
+    y_bad = ((y < 0) | (y > nrows - 1))
+    if y_bad.any():
+        y[y_bad] = 0
+    # linear indexing. z must be in 'C' order
+    ndx = np.floor(y) * ncols + np.floor(x)
+    ndx = ndx.astype('int32')
+    # fix parameters on x border
+    d = (x == ncols - 1)
+    x = (x - np.floor(x))
+    if d.any():
+        x[d] += 1
+        ndx[d] -= 1
+    # fix parameters on y border
+    d = (y == nrows - 1)
+    y = (y - np.floor(y))
+    if d.any():
+        y[d] += 1
+        ndx[d] -= ncols
+    # interpolate
+    one_minus_t = 1 - y
+    z = z.ravel()
+    f = (z[ndx] * one_minus_t + z[ndx + ncols] * y ) * (1 - x) + (
+        z[ndx + 1] * one_minus_t + z[ndx + ncols + 1] * y) * x
+    # Set out of range positions to extrapval
+    if x_bad.any():
+        f[x_bad] = extrapval
+    if y_bad.any():
+        f[y_bad] = extrapval
+    return f
+
 ### TODO: Add a general reweighting function
