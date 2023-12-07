34b96f4d1797ee929db7c51359f7054e9571d698
mpasha3 <mpasha3@asu.edu>
2023-12-06 19:39:02 -0500

    IsoTV weights

--- trips/utilities/weights.py
+++ trips/utilities/weights.py
@@ -27,15 +27,27 @@ Utility functions.
 """
 
 # Define IsoTV weights      
-def iso_TV_weights(x, nx, ny, epsilon, qnorm):
+def iso_TV_weights(x, u, nx, ny, e, q):
     nt = int((x.reshape((-1,1)).shape[0])/(nx*ny))
     Ls = first_derivative_operator_2d(nx, ny)
-    utemp = np.reshape(x, (nx*ny, nt))
-    Dutemp = Ls.dot(utemp)
-    wr = np.exp(2) * np.ones((2*nx*(ny), 1))
-    for i in range(2*nx*(ny)):
-        wr[i] = (np.linalg.norm(Dutemp[i,:])**2 + wr[i])**(qnorm/2-1)
-    wr = np.kron(np.ones((nt, 1)), wr)
+    spacen = int(Ls.shape[0] / 2)
+    spacent = spacen * nt
+    X = x.reshape(nx**2, nt)
+    LsX = Ls @ X
+    LsX1 = LsX[:spacen, :]
+    LsX2 = LsX[spacen:2*spacen, :]
+    weightx = (LsX1**2 + LsX2**2 + e**2)**((q-2) / 4)
+    weightx = np.concatenate((weightx.flatten(), weightx.flatten()))
+    weightt = (u[2*spacent:]**2 + e**2)**((q-2) / 4)
+    wr = np.concatenate((weightx.reshape(-1,1), weightt))
+    # nt = int((x.reshape((-1,1)).shape[0])/(nx*ny))
+    # Ls = first_derivative_operator_2d(nx, ny)
+    # utemp = np.reshape(x, (nx*ny, nt))
+    # Dutemp = Ls.dot(utemp)
+    # wr = np.exp(2) * np.ones((2*nx*(ny), 1))
+    # for i in range(2*nx*(ny)):
+    #     wr[i] = (np.linalg.norm(Dutemp[i,:])**2 + wr[i])**(qnorm/2-1)
+    # wr = np.kron(np.ones((nt, 1)), wr)
     return wr
 
 # Define GS weights
