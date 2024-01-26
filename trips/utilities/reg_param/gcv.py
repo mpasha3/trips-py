5aeda75ed925e0c9abe5bd1c19c351fe82689a96
mpasha3 <mpasha3@asu.edu>
2024-01-26 13:57:04 -0500

    gcv modified

--- trips/utilities/reg_param/gcv.py
+++ trips/utilities/reg_param/gcv.py
@@ -16,7 +16,7 @@ from scipy.optimize import newton, minimize
 import scipy.linalg as la
 import scipy.optimize as op
 from pylops import Identity, LinearOperator
-from utilities.utils import operator_qr, operator_svd, is_identity
+from utils import operator_qr, operator_svd, is_identity
 
 """
 Generalized crossvalidation
